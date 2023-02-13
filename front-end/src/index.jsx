9f5f1e9233751e26d253836ec418e1a1782c3cff
boloruyanga <boloruyangahop2@nest-35s-iMac.local>
2023-02-13 19:30:10 +0800

    footer

--- front-end/src/index.jsx
+++ front-end/src/index.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./assets/index.css";
 import { App } from "./pages/App";
+import { Footer } from "./pages/Footer";
 
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Footer />
   </React.StrictMode>
 );
 
--- front-end/tailwind.config.js
+++ front-end/tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      "dark-blue": "#252B3B",
+    },
   },
   plugins: [],
 };
