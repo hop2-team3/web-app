120465cc487aff9aeb1fe2e20401fd963fc83e8f
tsetsen <Tsetsenee@nest-52s-iMac.local>
2023-02-17 18:56:03 +0800

    header hiisim shigena

---
+++ front-end/src/components/Card.jsx
@@ -0,0 +1,14 @@
+// import React from 'react'
+// import star from '../assets/star.png'
+
+// export const Card = () => {
+//     return(
+//         <>
+//         <div className="h=[370px] w=[360px]" >
+//             <div className="flex flex-row">
+//                 <img src={star} alt='img'></img>
+//             </div>
+//         </div>
+//         </>
+//     )
+// }
