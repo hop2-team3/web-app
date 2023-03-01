5ba0e35c9725dd31f29502d562f5fc522d69add8
tsetsen <Tsetsenee@nest-52s-iMac.local>
2023-03-01 18:28:42 +0800

    Header

---
+++ front-end/src/pages/Services.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {Header} from "../components/Header"
+
+export const Services = () => {
+  return (
+    <>
+    <Header />
+    <div className='w-screen h-screen'></div>
+    </>
+  )
+}
